06a0c5e868740b4de191cfdce3bdc81a52bfa5dc
Min-Hyuk1229 <jang9934494@gmail.com>
2018-11-09 16:09:36 +0900

    source

---
+++ EarthQuake 최종/EarthQuake/Assets/Scripts/linkclick.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class linkclick : MonoBehaviour {
+    public void Uifunc()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=UuTowptYlrM");
+    }
+
+}
